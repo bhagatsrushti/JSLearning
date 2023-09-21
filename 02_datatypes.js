103659a2d1a95969ebd37ce1ce4d89d57eaad27e
srushtibhagat70 <srushtibhagat7083@gmail.com>
2023-09-21 09:27:57 +0530

    variablebasicas
    variablebasicas

---
+++ 02_datatypes.js
@@ -0,0 +1,41 @@
+// primitive data types
+
+var age = 32;
+console.log(typeof age);
+var price = 34.678;
+console.log(price, typeof price);
+
+var firstName = "Srushti";
+console.log(typeof firstName);
+var city = "Pune";
+console.log("City=>", city, "data type==>", typeof city);
+
+var hscMarks = "78";
+console.log(hscMarks, typeof hscMarks);
+
+var isMarried = true;
+console.log(isMarried, typeof isMarried);
+
+var isHandicap = false;
+console.log(isHandicap, typeof isHandicap);
+
+console.log(typeof "I love only js");
+console.log(typeof 20465);
+console.log(typeof false);
+
+console.log("----------null vs undefined----------");
+var accountNumber;
+console.log(accountNumber);
+accountNumber = null;
+console.log(accountNumber);
+
+var friendsCount = 7;
+console.log(friendsCount, typeof friendsCount);
+friendsCount = 'Seven';
+console.log(friendsCount, typeof friendsCount);
+
+// var firstName = 45;
+// String firstName = 500; 
+
+
+
---
+++ 02_variableBasicsAssig.js
@@ -0,0 +1,13 @@
+
+var PIN = 411001;
+console.log("PIN:", PIN, "type:",  typeof PIN);
+
+
+var familyName = "Bhagat";
+console.log( "FamilyName:",  familyName, "type:", typeof familyName);
+
+// var mcaMarks ="73.89";
+// console.log("mcaMarks:",   mcaMarks,"type:", typeof mcaMarks);
+
+var isPassed = true;
+console.log("isPassed:",   isPassed, "type:",  typeof isPassed);
