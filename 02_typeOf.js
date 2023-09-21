3659118e64ed2f03860e4a99650ac76e2d2faac4
srushtibhagat70 <srushtibhagat7083@gmail.com>
2023-09-21 10:13:46 +0530

    added some changes
    added some changes

---
+++ 02_typeOf.js
@@ -0,0 +1,3 @@
+console.log("----------1st stage-----------");
+
+var = bankName;
--- 02_variableBasicsAssig.js
+++ 02_variableBasicsAssig.js
@@ -1,13 +1,30 @@
-
+console.log("==============numberType=============");
 var PIN = 411001;
-console.log("PIN:", PIN, "type:",  typeof PIN);
+console.log("PIN:,", PIN, "type:",  typeof PIN);
 
-
+console.log("==============stringType=============");
 var familyName = "Bhagat";
-console.log( "FamilyName:",  familyName, "type:", typeof familyName);
+console.log( "FamilyName:,",  familyName, "type:", typeof familyName);
 
 // var mcaMarks ="73.89";
-// console.log("mcaMarks:",   mcaMarks,"type:", typeof mcaMarks);
+// console.log("mcaMarks:,",   mcaMarks,"type:", typeof mcaMarks);
 
+console.log("==============booleanType=============");
 var isPassed = true;
-console.log("isPassed:",   isPassed, "type:",  typeof isPassed);
+console.log("isPassed:,",   isPassed, "type:",  typeof isPassed);
+
+
+
+
+console.log("***************NUll vs Undefine***********");
+
+
+
+console.log("==============type1NULL==============")
+var valuea = null;   
+console.log(  "value a:,",valuea, "type:", typeof valuea );  
+
+console.log("===============type2Undefined===========");
+var valueb = undefined;
+console.log("value b:,", valueb,"type", typeof valueb);
+
