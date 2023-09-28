fdc215ce9a9b7fc716c7b9ae437963734f06dd2d
srushtibhagat70 <srushtibhagat7083@gmail.com>
2023-09-28 21:53:18 +0530

    stringBasicass01
    stringBasicass01

---
+++ 03_stringBasicAssign01.js
@@ -0,0 +1,27 @@
+console.log("********stringBasicAssign-01*************");
+
+console.log("******************************Step1******************************");
+
+function stringBasics() {
+    console.log(`My dream company is:   "Google Microsoft`);
+}
+stringBasics();
+console.log(``);
+
+console.log("*******************************Step2********************************");
+
+var hobby1 = "Exploring New places";
+var hobby2 = "Reading Books";
+var hobby3 = "Learning New things";
+
+console.log(
+  `My Hobbies Are :  ==    ${hobby1} 
+                     ==    ${hobby2}   
+                     ==    ${hobby3}`);
+
+var result = hobby1 + hobby2 + hobby3;
+console.log(``);
+
+console.log("***************************Step3*******************************");
+console.log(`Result of Strings concatenation is::: ${result}`);
+
