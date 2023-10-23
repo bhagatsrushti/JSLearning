aab21c4846773cf95abbeb09814a014858747601
srushtibhagat70 <srushtibhagat7083@gmail.com>
2023-10-23 20:18:15 +0530

    27_reduceAssignA.js
    27_reduceAssignA.js

--- 27_reduceAssignA.js
+++ 27_reduceAssignA.js
@@ -1,23 +1,21 @@
-console.log(`***********27_reduceAssign.js********`);
-console.log(`=====Step-1======`);
+console.log(`***********27_reduceAssignA.js********`);
+console.log(`==========Step-1============`);
 const array_numbers = [20,11,40,25,37,49,9,90,60,2,19];
 console.log(`Given Array are :[${array_numbers}]`);
 
-let sum = 0;
+let  sum=0;
 for (const element of array_numbers) {
-    sum = sum + element;
-    const arraySum =array_numbers.reduce((runningTotal,value)=>{
- return runningTotal + value;
-    })
+    sum= sum+element
 }
+console.log(`Sum of Given Array By Using Traditional For Loop : ${sum}`);
 console.log(`Sum of Given Array By using Reduce method :${sum}`);
 console.log(`__________________________________________________________`);
-console.log(`=====Step-2======`);
+console.log(`=======Step-2==========`);
 
 const arrayMultiply=array_numbers.filter((value)=>{
     return value%5==0;
     })
-    console.log(`Element Which Are Multiple Of "5" ===>>> ${arrayMultiply}`);
+    console.log(`Element Which Are Multiple Of "5" : ${arrayMultiply}`);
     const arrayMultiple5=arrayMultiply.reduce((runningValue,value1)=>{
         return runningValue  + value1
     })
