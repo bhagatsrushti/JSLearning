191c9790c73e63419cd6e30a3f0a466dcffaebaa
srushtibhagat70 <srushtibhagat7083@gmail.com>
2023-10-25 11:31:35 +0530

    30_domAssign.html
    30_domAssign.html

--- 27_reduce.js
+++ 27_reduce.js
@@ -18,3 +18,18 @@ const nestedArray =  [ [2, 5], [4, 5, 6],[ 2, 7, 11] ];
 const arrayNum = nestedArray.flatMap( (element => element));
 
 console.log(arrayNum);
+
+
+
+// const str = "How are you mate"; // "HoW ArE YoU MatE"
+// const arrayWord = str.split(' ');
+// let finalStr = '';
+// for (const word of arrayWord) {
+//     let firstChar = word.charAt(0).toUpperCase();
+//     let lastChar = word.charAt(word.length-1).toUpperCase();
+//     // Replace word,  first char by firstChar
+//     // Replace word,  last char by lastChar
+//     finalStr = finalStr + word;
+//     console.log(word, firstChar, lastChar );
+// }
+// console.log(finalStr);
